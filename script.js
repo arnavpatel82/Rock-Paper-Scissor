cad9738f1dd6cd3e506845f8b9086e5a2856ca37
arnavpatel82 <arnav.patel82@yahoo.com>
2022-07-14 17:38:26 +0800

    finished computerPlays and round functions

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function computerPlays(max) {
+    let num = Math.floor(Math.random() * max);
+
+    if (num === 0) {
+        return "Rock";
+
+    } else if (num === 1) {
+        return "Paper";
+
+    } else {
+        return "Scissor";
+    }
+}
+
+
+function round(playerSelection, computerSelection) {
+    playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let r = "Rock";
+    let p = "Paper";
+    let s = "Scissor";
+
+    if ((playerSelection === r && computerSelection === s) || (playerSelection === p && computerSelection === r) || playerSelection === s && computerSelection === p) {
+        return "You Win!";
+
+    } else if (playerSelection === computerSelection) {
+        return "It's a Tie";
+
+    } else {
+        return "You Lose!"
+    }
+}
+
+
+console.log(round("paper", "Paper"))
+
+
+
+
 
